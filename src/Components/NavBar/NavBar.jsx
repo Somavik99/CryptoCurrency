240d0921c55f0d8c8bda80ea830b9ecae8773bee
Avik Roy <somavik99@gmail.com>
2023-05-11 13:34:18 +0530

    main

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,30 @@
 import "./NavBar.css";
+import {TbCoinRupee} from "react-icons/tb"
+
 
 const NavBar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="Container">
+    <div className="MainNavContainer">
+      <h1 style={{display:"flex"}}><span style={{ transform:"rotate(-30deg)"}}><TbCoinRupee size={50} style={{color:"gold"}}/></span> Crypto Coins</h1>
+      <div className="CryptoContainer">
+        <h2 style={{cursor:"pointer"}}>Chart Data</h2>
+        <div>
+          <input
+            type="text"
+            placeholder="Crypto Search..."
+            style={{
+              width: "250px",
+              padding: "2.8%",
+              borderRadius: "0.5rem",
+              outline: "none",
+              fontSize:"20px"
+            }}
+            className="CryptoInput"
+          />
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
